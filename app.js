3fbd71b507e51217216bdabc8cd33a155a4675f8
essennejaye <sjeffers02@gmail.com>
2023-07-05 22:55:20 -0700

    express_server example

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('express_server/static', express.static('public'));
 
-app.use('/simple/', indexRouter);
-app.use('/simple/', usersRouter);
+app.use('/express_server', indexRouter);
+app.use('/express_server', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
